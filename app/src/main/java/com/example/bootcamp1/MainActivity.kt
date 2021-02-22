f9b10e45788cb052eb1e83480a675acea23e7490
Varsha <varsha.vaidywan@tothenew.com>
2021-02-22 13:51:24 +0530

    previous deleted

--- app/src/main/java/com/example/bootcamp1/MainActivity.kt
+++ app/src/main/java/com/example/bootcamp1/MainActivity.kt
@@ -9,66 +9,66 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //ques1
-        var obj1 = q1()
-        obj1.print()
-
-        //ques2
-        var obj2 = q2()
-        var a : Int = 10
-        var b : Int = 20
-        var c : Double = 35.67
-        var d : Double = 12.3
-        var str1 : String = "hello this is ..."
-        var str2 : String = "Varsha !!"
-        var str3 : String = "...How u doin"
-        obj2.function(a, b)
-        obj2.function(c, d)
-        obj2.function()
-        obj2.function(str1, str2)
-        obj2.function(str1, str2, str3)
-
-        //ques3
-        var o1 = bank()
-        o1.getDetails()
-        var o2 = SBI()
-        o2.getDetails()
-        var o3 = BOI()
-        o3.getDetails()
-        var o4 = ICICI()
-        o4.getDetails()
-
-        //ques4
-        var ob1 = student()
-        ob1.setId()
-        println("student's id : " + ob1.id)
-        var ob2 = library()
-        ob2.setName()
-        println("name : " + ob2.name)
-
-        //ques5
-        var ob3 = marks()
-        ob3.m = 55
-        ob3.marksCalculation(ob3.m)
-        ob3.m = 68
-        ob3.marksCalculation(ob3.m)
-        ob3.m = 72
-        ob3.marksCalculation(ob3.m)
-        ob3.m = 99
-        ob3.marksCalculation(ob3.m)
-        ob3.m = 40
-        ob3.marksCalculation(ob3.m)
-
-        //ques6
-        var ob4 = q6()
-        ob4.doit()
-
-        //ques7
-        var ob5 = q7()
-        ob5.func()
-
-        //ques8
-        var ob6 = q8()
-        ob6.func()
+//        //ques1
+//        var obj1 = q1()
+//        obj1.print()
+//
+//        //ques2
+//        var obj2 = q2()
+//        var a : Int = 10
+//        var b : Int = 20
+//        var c : Double = 35.67
+//        var d : Double = 12.3
+//        var str1 : String = "hello this is ..."
+//        var str2 : String = "Varsha !!"
+//        var str3 : String = "...How u doin"
+//        obj2.function(a, b)
+//        obj2.function(c, d)
+//        obj2.function()
+//        obj2.function(str1, str2)
+//        obj2.function(str1, str2, str3)
+//
+//        //ques3
+//        var o1 = bank()
+//        o1.getDetails()
+//        var o2 = SBI()
+//        o2.getDetails()
+//        var o3 = BOI()
+//        o3.getDetails()
+//        var o4 = ICICI()
+//        o4.getDetails()
+//
+//        //ques4
+//        var ob1 = student()
+//        ob1.setId()
+//        println("student's id : " + ob1.id)
+//        var ob2 = library()
+//        ob2.setName()
+//        println("name : " + ob2.name)
+//
+//        //ques5
+//        var ob3 = marks()
+//        ob3.m = 55
+//        ob3.marksCalculation(ob3.m)
+//        ob3.m = 68
+//        ob3.marksCalculation(ob3.m)
+//        ob3.m = 72
+//        ob3.marksCalculation(ob3.m)
+//        ob3.m = 99
+//        ob3.marksCalculation(ob3.m)
+//        ob3.m = 40
+//        ob3.marksCalculation(ob3.m)
+//
+//        //ques6
+//        var ob4 = q6()
+//        ob4.doit()
+//
+//        //ques7
+//        var ob5 = q7()
+//        ob5.func()
+//
+//        //ques8
+//        var ob6 = q8()
+//        ob6.func()
     }
 }
